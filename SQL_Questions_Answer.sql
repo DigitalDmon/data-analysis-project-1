7a7d1dcdc0f2393e44f893b7a6be136f69b734e9
DigitalDmon <eladio.gonzalez@utp.ac.pa>
2025-04-20 00:37:19 -0500

    Files to do the project

---
+++ SQL_Questions_Answer.sql
@@ -0,0 +1,105 @@
+/*
+DROP TABLE df_orders
+
+CREATE TABLE df_orders (	
+	[order_id] INT PRIMARY KEY,
+	[order_date] DATE,
+	[ship_mode] VARCHAR(20),
+	[segment] VARCHAR(20),
+	[country] VARCHAR(20),
+	[city] VARCHAR(20),
+	[state] VARCHAR(20),
+	[postal_code] VARCHAR(20),
+	[region] VARCHAR(20),
+	[category] VARCHAR(20),
+	[sub_category] VARCHAR(20),
+	[product_id] VARCHAR(50),
+	[quantity] INT,
+	[discount] DECIMAL(7,2),
+	[sale_price] DECIMAL(7,2),
+	[profit] DECIMAL(7,2)
+)
+*/
+
+-- THIS TABLE IS IN MASTER DATABASE
+
+SELECT * FROM df_orders
+
+-- FIND TOP 10 HIGHEST REVENUE GERERATING PRODUCTS
+SELECT TOP 10 product_id, SUM(sale_price) AS sales
+FROM df_orders
+GROUP BY product_id
+ORDER BY sales DESC
+
+-- FIND TOP 5 HIGHEST SELLING PRODUCTS IN EACH REGION
+WITH cte as (
+	SELECT region, product_id, SUM(sale_price) AS sales
+	FROM df_orders
+	GROUP BY region, product_id
+)
+SELECT * FROM (
+	SELECT * ,
+	ROW_NUMBER() OVER(PARTITION BY region ORDER BY sales DESC) AS rn
+	FROM cte
+) A
+WHERE rn <= 5
+
+-- FIND MONTH OVER MONTH GROWTH COMPARISON FOR 2022 AND 2023 SALES EG: JAN 2022 VS JAN 2023
+WITH cte AS (
+	SELECT
+		YEAR(order_date) AS order_year,
+		MONTH(order_date) AS order_month,
+		SUM(sale_price) AS sales
+	FROM df_orders
+	GROUP BY 
+		YEAR(order_date),
+		MONTH(order_date)
+)
+SELECT 
+	order_month,
+	SUM(CASE WHEN order_year = 2022 THEN sales ELSE 0 END) AS sales_2022,
+	sum(CASE WHEN order_year = 2023 THEN sales ELSE 0 END) AS sales_2023
+FROM cte
+GROUP BY order_month
+ORDER BY order_month
+
+-- FOR EACH CATEGORY WICH MONTH HAD HIGHEST SALES
+WITH cte AS (
+	SELECT 
+		category,
+		FORMAT(order_date, 'yyyy - MM') AS order_year_month,
+		SUM(sale_price) AS sales
+	FROM df_orders
+	GROUP BY 
+		category,
+		FORMAT(order_date, 'yyyy - MM')
+)
+SELECT * 
+FROM (
+	SELECT * , ROW_NUMBER() OVER(PARTITION BY category ORDER BY sales DESC) AS rn
+	FROM cte
+) a
+WHERE rn = 1
+
+-- WHICH SUB CATEGORY HAD HIGHEST GROWTH BY PROFIT IN 2023 COMPARE TO 2022
+WITH cte AS (
+	SELECT
+		sub_category,
+		YEAR(order_date) AS order_year,
+		SUM(sale_price) AS sales
+	FROM df_orders
+	GROUP BY 
+		sub_category,
+		YEAR(order_date)
+), 
+cte2 AS (
+	SELECT 
+		sub_category,
+		SUM(CASE WHEN order_year = 2022 THEN sales ELSE 0 END) AS sales_2022,
+		sum(CASE WHEN order_year = 2023 THEN sales ELSE 0 END) AS sales_2023
+	FROM cte
+	GROUP BY sub_category
+)
+SELECT TOP 1 * , (sales_2023 - sales_2022)
+FROM cte2
+ORDER BY (sales_2023 - sales_2022) DESC
